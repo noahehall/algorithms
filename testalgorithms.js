ae68e27995e089cdb2de236b70b2a00142125041
Noah Edward Hall <noahedwardhall@gmail.com>
2017-02-15 20:24:42 -0800

    add list to testalgorithms

--- testalgorithms.js
+++ testalgorithms.js
@@ -1,9 +1,12 @@
 var dsalgo = require('./index.js');
 
 var twod = new dsalgo.arrays.TwoD(5, 5, 5);
+var list = new dsalgo.lists.List();
+
 console.dir([
   dsalgo,
   dsalgo.linear.plus(222220, 111911),
   twod.calculateRowStats(false),
-  twod.calculateColumnStats()
+  twod.calculateColumnStats(),
+  list
 ]);
