d542daab3d8adf8c87e87154819c9a4eeeaed90d
Noah Edward Hall <noahedwardhall@gmail.com>
2017-02-15 19:25:27 -0800

    implement List ADT

--- src/datastructures/lists/list.js
+++ src/datastructures/lists/list.js
@@ -6,4 +6,62 @@ export default class List {
   constructor() {
     return this;
   }
+
+  // properties
+  listSize = 0;
+  pos = 0;
+  dataStore = [];
+
+  // behavior
+  // CREATE
+    insert = () => {
+      // TODO
+    append = () => {
+      // TODO
+    }
+
+  // READ
+    find = () => {
+      // TODO
+    }
+    toString = () => {
+      // TODO
+    }
+    length = () => {
+      // TODO
+    }
+    currentPosition = () => {
+      // TODO
+    }
+    getElement = () => {
+      // TODO
+    }
+    contains = () => {
+      // TODO
+    }
+  // UPDATE
+    front = () => {
+      // TODO
+    }
+    end = () => {
+      // TODO
+    }
+    previous = () => {
+      // TODO
+    }
+    next = () => {
+      // TODO
+    }
+    moveTo = () => {
+      // TODO
+    }
+  // DELETE
+    clear = () => {
+      // TODO
+    }
+    remove = () => {
+
+    }
+
+
 }
