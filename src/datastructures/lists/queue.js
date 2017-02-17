3673c3e7a87f06ba750aa867ec557696e7dbde0a
Noah Edward Hall <noahedwardhall@gmail.com>
2017-02-16 23:33:41 -0800

    add queue class

---
+++ src/datastructures/lists/queue.js
@@ -0,0 +1,17 @@
+import List from './list.js';
+
+/**
+ * creates a Queue
+ * @see datastructures and algorithms in javascript, page 59
+ */
+export default class Queue extends List {
+  constructor() {
+    super();
+  }
+
+  // behavior
+  // CREATE
+  // READ
+  // UPDATE
+  // DELETE
+}
