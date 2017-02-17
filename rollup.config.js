569b68746f6bebdb4e67aa15de400cb1c770bb63
Noah Edward Hall <noahedwardhall@gmail.com>
2017-02-17 00:03:45 -0800

    add uglify and progress

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,16 @@
 import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import progress from 'rollup-plugin-progress';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: './src/index.js',
   dest: 'index.js',
-  format: 'cjs',
+  format: 'umd',
+  moduleName: 'dsalgo',
   plugins: [
+    progress({ clearLine: false }),
     nodeResolve({
       jsnext: true,
       main: true
@@ -20,6 +24,7 @@ export default {
       exclude: 'node_modules/**',
       plugins: ['external-helpers'],
       runtimeHelpers: true
-    })
+    }),
+    uglify()
   ]
 };
